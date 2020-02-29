15394a61c92ede93f735979b67df845d93e0dcc4
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2020-02-29 13:09:17 +0530

    Linted Code and Remove Bugs
    - moved decorator from class to outside in the global namespace.
    - add <type> in docstring

--- utils.py
+++ utils.py
@@ -6,13 +6,36 @@ import _sha256
 import _sha512
 
 
-class HashBytes(object):
+def check_bytes(func):
+    """
+        Decorator which checks if the input to the hash function is bytes.
+        Args:
+            func <Callable>: A hash function accepting bytes as input.
+        Returns:
+            <Callable>: Wrapper which checks for correctness and evaluate the results.
+    """
+    def inner(_input, *args, **kwargs):
+        """
+        Actual wrapper which will check if the input is bytes.
+        Args:
+            _input: bytes input
+        Raises:
+            TypeError: if the _input is not bytes.
+        """
+        try:
+            return func(_input, *args, **kwargs)
+        except TypeError:
+            raise TypeError("Expected a Byte Like Object")
+    return inner
+
+
+class HashBytes:
     """Hashing ByteLike objects"""
     def __init__(self, return_type=str):
         """
         Class to bind all the methods associated with Hashing Bytes together
         Args:
-            return_type: can be either str or bytes.
+            return_type <type>: can be either str or bytes.
         Raises:
             TypeError: if return_type is not str or bytes.
         """
@@ -20,41 +43,18 @@ class HashBytes(object):
             raise TypeError("Expected either bytes or str")
         self.return_type = return_type
 
-    @classmethod
-    def check_bytes(cls, func):
-        """
-            Decorator which checks if the input to the hash function is bytes.
-            Args:
-                func: A callable hash function accepting bytes as input.
-            Returns:
-                Wrapper which checks for correctness and evaluate the results.
-        """
-        def inner(_input, *args, **kwargs):
-            """
-            Actual wrapper which will check if the input is bytes.
-            Args:
-                _input: bytes input
-            Raises:
-                TypeError: if the _input is not bytes.
-            """
-            try:
-                return func(_input, *args, **kwargs)
-            except:
-                raise TypeError("Expected a Byte Like Object")
-        return inner
 
     def adj_return_type(self, hash_bytes):
         """
         Adjusts the return type according the demand of user.
         Args:
-            hash_bytes: byte like object.
+            hash_bytes <bytes>: byte like object.
         Returns:
-            byte or str representation of the input as defined in the instance.
+            <byte/str>: result according to self.return_type
         """
         if self.return_type == str:
             return hash_bytes.hexdigest()
-        else:
-            return hash_bytes
+        return hash_bytes.digest()
 
     @check_bytes
     def sha256(self, data):
@@ -71,24 +71,29 @@ class HashStrings(HashBytes):
     """
     Given a String, Hash it.
     """
-
+    def __init__(self, return_type=str):
+        super().__init__()
     @classmethod
     def convert_to_bytes(cls, ip_str):
         """
         Convert the input string to bytes.
         Args:
-            ip_str: string to be converted to bytes
+            ip_str <str>: string to be converted to bytes
+        Returns:
+            <bytes>: byte representation of the ip_str.
         """
         try:  # EAFP
             return str.encode(ip_str)
-        except:
+        except ValueError:
             raise ValueError("String expected. Found {}".format(type(ip_str)))
 
-    sha256 = lambda self, ip_str: super().sha256(self.convert_to_bytes(ip_str))
-    sha512 = lambda self, ip_str: super().sh512(self.convert_to_bytes(ip_str))
+    def sha256(self, data):
+        return super().sha256(self.convert_to_bytes(data))
+    def sha512(self, data):
+        return super().sha512(self.convert_to_bytes(data))
 
 
-class Node(object):
+class Node:
     """
     Basic Blockchain Node.
     """
@@ -104,13 +109,10 @@ class Node(object):
 
     def mine(self):
         """Abstract method yet to be implemented"""
-        pass
 
     def validate_chain(self):
         """Not Implemented Yet"""
-        pass
 
     def get_acc_info(self):
         """Not Implemented Yet"""
-        pass
 
